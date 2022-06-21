ab1180c48a151084a298325a4424f3e75d25dc1c
jetfire679 <jetfire679@gmail.com>
2022-06-21 12:49:10 -0400

    testing2

--- network-us-east-1.tf
+++ network-us-east-1.tf
@@ -10,7 +10,7 @@ module "vpc" {
     aws = aws.east-1
    }
 
-  name = join("-", [var.vApp,"vpc"])
+  name = join("-", [var.vApp,"vpc-east-1"])
   cidr = "10.1.0.0/16"
 
 
@@ -33,48 +33,48 @@ module "vpc" {
 }
 
 #Get all available AZ's in VPC for master region
-data "aws_availability_zones" "azs" {
+data "aws_availability_zones" "azs-east-1" {
   state = "available"
 }
 
 
-resource "aws_ssm_parameter" "public_subnet_id_a" {
+resource "aws_ssm_parameter" "public_subnet_id_a-east-1" {
   name  = join("-", [var.vApp, "PubSubA_ID"])
   type  = "String"
   value = module.vpc.public_subnets[0]
 }
 
-resource "aws_ssm_parameter" "public_subnet_id_b" {
+resource "aws_ssm_parameter" "public_subnet_id_b-east-1" {
   name  = join("-", [var.vApp, "PubSubB_ID"])
   type  = "String"
   value = module.vpc.public_subnets[1]
 }
 
-resource "aws_ssm_parameter" "public_subnet_id_c" {
+resource "aws_ssm_parameter" "public_subnet_id_c-east-1 {
   name  = join("-", [var.vApp, "PubSubC_ID"])
   type  = "String"
   value = module.vpc.public_subnets[2]
 }
 
-resource "aws_ssm_parameter" "public_subnet_a" {
+resource "aws_ssm_parameter" "public_subnet_a-east-1" {
   name  = join("-", [var.vApp, "PubSubA"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[0]
 }
 
-resource "aws_ssm_parameter" "public_subnet_b" {
+resource "aws_ssm_parameter" "public_subnet_b-east-1" {
   name  = join("-", [var.vApp, "PubSubB"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[1]
 }
 
-resource "aws_ssm_parameter" "public_subnet_c" {
+resource "aws_ssm_parameter" "public_subnet_c-east-1" {
   name  = join("-", [var.vApp, "PubSubC"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[2]
 }
 
-resource "aws_ssm_parameter" "vpc_id" {
+resource "aws_ssm_parameter" "vpc_id-east-1" {
   name  = join("-", [var.vApp, "VpcId"])
   type  = "String"
   value = module.vpc.vpc_id
--- network-us-east-2.tf
+++ network-us-east-2.tf
@@ -9,7 +9,7 @@ module "vpc" {
   providers = {
     aws = aws.east-2
    }
-  name = join("-", [var.vApp,"vpc"])
+  name = join("-", [var.vApp,"vpc-east-1"])
   cidr = "10.0.0.0/16"
 
 
@@ -32,48 +32,48 @@ module "vpc" {
 }
 
 #Get all available AZ's in VPC for master region
-data "aws_availability_zones" "azs" {
+data "aws_availability_zones" "azs-east-2" {
   state = "available"
 }
 
 
-resource "aws_ssm_parameter" "public_subnet_id_a" {
+resource "aws_ssm_parameter" "public_subnet_id_a-east-2" {
   name  = join("-", [var.vApp, "PubSubA_ID"])
   type  = "String"
   value = module.vpc.public_subnets[0]
 }
 
-resource "aws_ssm_parameter" "public_subnet_id_b" {
+resource "aws_ssm_parameter" "public_subnet_id_b-east-2" {
   name  = join("-", [var.vApp, "PubSubB_ID"])
   type  = "String"
   value = module.vpc.public_subnets[1]
 }
 
-resource "aws_ssm_parameter" "public_subnet_id_c" {
+resource "aws_ssm_parameter" "public_subnet_id_c-east-2" {
   name  = join("-", [var.vApp, "PubSubC_ID"])
   type  = "String"
   value = module.vpc.public_subnets[2]
 }
 
-resource "aws_ssm_parameter" "public_subnet_a" {
+resource "aws_ssm_parameter" "public_subnet_a-east-2" {
   name  = join("-", [var.vApp, "PubSubA"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[0]
 }
 
-resource "aws_ssm_parameter" "public_subnet_b" {
+resource "aws_ssm_parameter" "public_subnet_b-east-2" {
   name  = join("-", [var.vApp, "PubSubB"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[1]
 }
 
-resource "aws_ssm_parameter" "public_subnet_c" {
+resource "aws_ssm_parameter" "public_subnet_c-east-2" {
   name  = join("-", [var.vApp, "PubSubC"])
   type  = "String"
   value = module.vpc.public_subnets_cidr_blocks[2]
 }
 
-resource "aws_ssm_parameter" "vpc_id" {
+resource "aws_ssm_parameter" "vpc_id-east-2" {
   name  = join("-", [var.vApp, "VpcId"])
   type  = "String"
   value = module.vpc.vpc_id

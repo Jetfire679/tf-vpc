3d131794dcd36ebd952f256a0f3803c084a1f476
jetfire679 <jetfire679@gmail.com>
2022-06-21 14:39:00 -0400

    peering1

--- peering.tf
+++ peering.tf
@@ -1,50 +1,59 @@
-# data "aws_ssm_parameter" "rlv-VpcId-east1" {
-#   name = "rlv-VpcId"
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region  = "us-east-1"
+  alias = "east-1"
+}
 
-# data "aws_ssm_parameter" "rlv-VpcId-east2" {
-#   name = "rlv-VpcId"
-#   region = "us-east-2"
-# }
+provider "aws" {
+  region  = "us-east-2"
+  alias = "east-2"
+}
 
-# data.aws_ssm_parameter.rlv-VpcId.value
+data "aws_ssm_parameter" "rlv-VpcId-east1" {
+  name = "rlv-VpcId"
+  provider = aws.east-1
+}
 
-# resource "aws_vpc_peering_connection" "primary2secondary" {
-#   # Main VPC ID.
-#   vpc_id = data.aws_ssm_parameter.rlv-VpcId-east2.value
+data "aws_ssm_parameter" "rlv-VpcId-east2" {
+  name = "rlv-VpcId"
+  region = "us-east-2"
+  provider = aws.east-2
+}
 
-#   # AWS Account ID. This can be dynamically queried using the
-#   # aws_caller_identity data resource.
-#   # https://www.terraform.io/docs/providers/aws/d/caller_identity.html
-#   peer_owner_id = "${data.aws_caller_identity.current.account_id}"
+resource "aws_vpc_peering_connection" "primary2secondary" {
+  # Main VPC ID.
+  vpc_id = data.aws_ssm_parameter.rlv-VpcId-east2.value
 
-#   # Secondary VPC ID.
-#   peer_vpc_id = data.aws_ssm_parameter.rlv-VpcId-east1.value
+  # AWS Account ID. This can be dynamically queried using the
+  # aws_caller_identity data resource.
+  # https://www.terraform.io/docs/providers/aws/d/caller_identity.html
+  peer_owner_id = data.aws_caller_identity.current.account_id
 
-#   # Flags that the peering connection should be automatically confirmed. This
-#   # only works if both VPCs are owned by the same account.
-#   auto_accept = true
-# }
+  # Secondary VPC ID.
+  peer_vpc_id = data.aws_ssm_parameter.rlv-VpcId-east1.value
 
-# resource "aws_route" "primary2secondary" {
-#   # ID of VPC 1 main route table.
-#   route_table_id = "${aws_vpc.primary.main_route_table_id}"
+  # Flags that the peering connection should be automatically confirmed. This
+  # only works if both VPCs are owned by the same account.
+  auto_accept = true
+}
 
-#   # CIDR block / IP range for VPC 2.
-#   destination_cidr_block = "${aws_vpc.secondary.cidr_block}"
+resource "aws_route" "primary2secondary" {
+  # ID of VPC 1 main route table.
+  route_table_id = module.vpc-east-1.vpc_main_route_table_id 
 
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
-# }
+  # CIDR block / IP range for VPC 2.
+  destination_cidr_block = module.vpc-east-2.vpc_cidr_block
 
-# resource "aws_route" "secondary2primary" {
-#   # ID of VPC 2 main route table.
-#   route_table_id = "${aws_vpc.secondary.main_route_table_id}"
+  # ID of VPC peering connection.
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
+}
 
-#   # CIDR block / IP range for VPC 2.
-#   destination_cidr_block = "${aws_vpc.primary.cidr_block}"
+resource "aws_route" "secondary2primary" {
+  # ID of VPC 2 main route table.
+  route_table_id = module.vpc-east-2.vpc_main_route_table_id 
 
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
-# }
+  # CIDR block / IP range for VPC 2.
+  destination_cidr_block = module.vpc-east-1.vpc_cidr_block
+
+  # ID of VPC peering connection.
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
+}

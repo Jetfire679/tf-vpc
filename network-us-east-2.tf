673973875535e250768e6de4f7876af6a8f99271
jetfire679 <jetfire679@gmail.com>
2022-06-21 13:05:43 -0400

    testing7

--- network-us-east-2.tf
+++ network-us-east-2.tf
@@ -39,41 +39,41 @@ data "aws_availability_zones" "azs-east-2" {
 resource "aws_ssm_parameter" "public_subnet_id_a-east-2" {
   name  = join("-", [var.vApp, "PubSubA_ID"])
   type  = "String"
-  value = module.vpc.public_subnets[0]
+  value = module.vpc-east-2.public_subnets[0]
 }
 
 resource "aws_ssm_parameter" "public_subnet_id_b-east-2" {
   name  = join("-", [var.vApp, "PubSubB_ID"])
   type  = "String"
-  value = module.vpc.public_subnets[1]
+  value = module.vpc-east-2.public_subnets[1]
 }
 
 resource "aws_ssm_parameter" "public_subnet_id_c-east-2" {
   name  = join("-", [var.vApp, "PubSubC_ID"])
   type  = "String"
-  value = module.vpc.public_subnets[2]
+  value = module.vpc-east-2.public_subnets[2]
 }
 
 resource "aws_ssm_parameter" "public_subnet_a-east-2" {
   name  = join("-", [var.vApp, "PubSubA"])
   type  = "String"
-  value = module.vpc.public_subnets_cidr_blocks[0]
+  value = module.vpc-east-2.public_subnets_cidr_blocks[0]
 }
 
 resource "aws_ssm_parameter" "public_subnet_b-east-2" {
   name  = join("-", [var.vApp, "PubSubB"])
   type  = "String"
-  value = module.vpc.public_subnets_cidr_blocks[1]
+  value = module.vpc-east-2.public_subnets_cidr_blocks[1]
 }
 
 resource "aws_ssm_parameter" "public_subnet_c-east-2" {
   name  = join("-", [var.vApp, "PubSubC"])
   type  = "String"
-  value = module.vpc.public_subnets_cidr_blocks[2]
+  value = module.vpc-east-2.public_subnets_cidr_blocks[2]
 }
 
 resource "aws_ssm_parameter" "vpc_id-east-2" {
   name  = join("-", [var.vApp, "VpcId"])
   type  = "String"
-  value = module.vpc.vpc_id
+  value = module.vpc-east-2.vpc_id
 }

8ae02c6dbd12606fde39bd3913b9c0899ebb9e91
jetfire679 <jetfire679@gmail.com>
2022-06-22 10:43:33 -0400

    updates11

--- provider.tf
+++ provider.tf
@@ -4,3 +4,13 @@
 provider "aws" {
   region                  = var.aws_region
 }
+
+provider "aws" {
+  region  = "us-east-2"
+  alias = "east-2"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  alias = "east-1"
+}

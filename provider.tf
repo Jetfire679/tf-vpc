f50ea6970e325958007c11ea566d406a586adc50
jetfire679 <jetfire679@gmail.com>
2022-06-22 13:19:07 -0400

    updates20

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# provider.tf
+
+# Specify the provider and access details
+# provider "aws" {
+#   region                  = var.aws_region
+# }
+
+provider "aws" {
+  region  = "us-east-2"
+  alias = "east-2"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  alias = "east-1"
+}

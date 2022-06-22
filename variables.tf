e31d8df349df4e867b5d3285dfda354e429dd536
jetfire679 <jetfire679@gmail.com>
2022-06-22 10:25:18 -0400

    updates7

--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "vApp" {
   type    = string
   default = "rlv"
 }
+
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "requestor_vpc_cidr" {
+  type        = string
+  description = "Requestor VPC CIDR"
+}
+
+variable "acceptor_vpc_cidr" {
+  type        = string
+  description = "Acceptor VPC CIDR"
+}

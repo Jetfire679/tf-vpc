fe005e415337d5a7954991850d601c358824c644
jetfire679 <jetfire679@gmail.com>
2022-06-22 13:25:45 -0400

    adding routes

--- peering.tf
+++ peering.tf
@@ -35,34 +35,32 @@ resource "aws_vpc_peering_connection_accepter" "accepter" {
   ]      
 }
 
+resource "aws_route" "primary2secondary" {
+  # ID of VPC 1 main route table.
+  route_table_id = module.vpc-east-1.vpc_main_route_table_id 
 
+  provider = aws.east-2
 
-# resource "aws_route" "primary2secondary" {
-#   # ID of VPC 1 main route table.
-#   route_table_id = module.vpc-east-1.vpc_main_route_table_id 
+  # CIDR block / IP range for VPC 2.
+  destination_cidr_block = module.vpc-east-2.vpc_cidr_block
 
-#   provider = aws.east-2
-
-#   # CIDR block / IP range for VPC 2.
-#   destination_cidr_block = module.vpc-east-2.vpc_cidr_block
-
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
-# }
+  # ID of VPC peering connection.
+  vpc_peering_connection_id = aws_vpc_peering_connection.requester.id
+}
 
 
 
 
 
-# resource "aws_route" "secondary2primary" {
-#   # ID of VPC 2 main route table.
-#   route_table_id = module.vpc-east-2.vpc_main_route_table_id 
+resource "aws_route" "secondary2primary" {
+  # ID of VPC 2 main route table.
+  route_table_id = module.vpc-east-2.vpc_main_route_table_id 
 
-#   provider = aws.east-1
+  provider = aws.east-1
 
-#   # CIDR block / IP range for VPC 2.
-#   destination_cidr_block = module.vpc-east-1.vpc_cidr_block
+  # CIDR block / IP range for VPC 2.
+  destination_cidr_block = module.vpc-east-1.vpc_cidr_block
 
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
-# }
+  # ID of VPC peering connection.
+  vpc_peering_connection_id = aws_vpc_peering_connection.requester.id
+}
